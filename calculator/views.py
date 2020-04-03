b7063bc696d88872e397fd0b036c736264c40ffb
jlw <1051032022@qq.com>
2020-04-03 20:24:31 +0800

    验证表达式，TODO:改用正则表达式试试

--- calculator/views.py
+++ calculator/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.forms import model_to_dict
 from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse, redirect
 from .models import History, MyUser
 import logging
+from .utils import check_expression
+from lib.exception import GrammarError,IllegalCharacter
 
 logger = logging.getLogger("log")
 
@@ -24,7 +27,7 @@ def result(request):
     :return:
     """
     result = request.GET.get("result",0)
-    input_str = request.GET.get("input_str","").replace("2B%","+")
+    input_str = request.GET.get("input_str", "").replace("2B%","+")
     history = History.objects.filter(myuser__user=request.user)
     logger.info("{} 查看了历史记录".format(request.user.username))
     return render(request,"calculator/result.html",
@@ -67,17 +70,22 @@ def user_register(request):
             user.save()
             myUser = MyUser.objects.create(user=user,sex=sex)
 
-
+        except IntegrityError as e:
+            status = "false"
+            message = "用户名重复"
+            logger.info("{} 注册失败！{}".format(username,repr(e)))
         except Exception as e:
             status = "false"
             message = repr(e)
-            logger.info("{} 注册失败！{}".format(username,repr(e)))
+            logger.error("{} 注册失败！{}".format(username,repr(e)))
         else:
             status = "true"
             message = "成功注册！"
+            login(request, user)
         return JsonResponse({"status":status,"message":message})
 
 
+@login_required
 def user_logout(request):
     """
     注销
@@ -97,12 +105,22 @@ def calculate(request):
     :param request:
     :return:
     """
-    input_str = request.POST.get("input_str")
+    input_str = request.POST.get("input_str","")
+    if not input_str:
+        return JsonResponse({"status": "false", "message": "输入表达式不能为空"})
+
+    try:
+        check_expression(input_str)
+    except GrammarError as e:
+        return JsonResponse({"status": "false", "message": e.detail})
+    except IllegalCharacter as e:
+        return JsonResponse({"status": "false", "message": e.detail})
+
     try:
         result = eval(input_str)
     except Exception as e:
-        return JsonResponse({"status": "false", "message": str(e)})
-    myuser = MyUser.objects.get(user=request.user)
-    history = History.objects.create(myuser=myuser,input_str=input_str,result=result)
+        return JsonResponse({"status": "false", "message": repr(e)})
+    # myuser = MyUser.objects.filter(user=request.user).first()
+    history = History.objects.create(myuser=request.user.myuser,input_str=input_str,result=result)
 
     return JsonResponse({"status": "true", "message": model_to_dict(history)})
